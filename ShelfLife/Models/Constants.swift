a4754d0604b788a1414152dd13ec3256dde3b140
ari-nair-44 <arinair44@gmail.com>
2024-06-15 15:22:41 -0400

    Change 1

--- ShelfLife/Models/Constants.swift
+++ ShelfLife/Models/Constants.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class Constants {
     
-    var foodExpiredColor = UIColor(red: 199/255, green: 108/255, blue: 108/255, alpha: 1)
-    var foodGoodColor = UIColor(red: 52/255, green: 209/255, blue: 219/255, alpha: 1)
+    var foodExpiredColor = UIColor(red: 199/255, green: 108/255, blue: 108/255, alpha: 0.5)
+    var foodGoodColor = UIColor(red: 52/255, green: 209/255, blue: 219/255, alpha: 0)
     
     var iconFridgeLight = "icon-fridge-light"
     var iconFreezerLight = "icon-freezer-light"
